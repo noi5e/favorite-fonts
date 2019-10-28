876a095a76701760990f61e0ba3b429640b16a30
Will Gutierrez <will.gutierrez@gmail.com>
2019-10-28 14:09:21 -0700

    require axios in api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,12 @@
 const express = require('express');
 let router = new express.Router();
 
+const axios = require('axios');
+
 router.get('/get_all_fonts', (request, response, next) => {
+
+  // https://www.googleapis.com/webfonts/v1/webfonts?key=YOUR-API-KEY
+  // https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity
 
 }); 
 
