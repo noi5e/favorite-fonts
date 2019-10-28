091b720a48e819fe661dd903835484e46d143ebf
Will Gutierrez <will.gutierrez@gmail.com>
2019-10-28 14:05:14 -0700

    add custom route for /api

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const http = require('http').Server(app);
 // static files
 app.use(express.static('./dist'));
 
+// custom routes
+const api = require('./server/routes/api');
+app.use('/api', api);
+
 // serve index.html
 app.get('/*', function(request, response, next) {
   response.sendFile(path.join(__dirname, '/dist/index.html'));
---
+++ server/routes/api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+let router = new express.Router();
+
+router.get('/get_all_fonts', (request, response, next) => {
+
+}); 
+
+module.exports = router;
