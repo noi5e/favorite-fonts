843ea0c0f385917fcbc454faad1d7b471603bd20
Will Gutierrez <will.gutierrez@gmail.com>
2019-10-29 13:20:29 -0700

    change response from /get_all_fonts request so that it sends font data straight from Google API

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,10 +5,29 @@ const axios = require('axios');
 
 router.get('/get_all_fonts', (request, response, next) => {
 
+  const fetchGoogleFonts = async() => {
+    try {
+      const result = await axios.get('https://www.googleapis.com/webfonts/v1/webfonts', {
+        params: {
+          key: process.env.GOOGLE_FONTS_DEVELOPER_API_KEY,
+          sort: 'popularity'
+        }
+      });
+
+      response.send(result.data.items)
+    } catch(error) {
+      console.log('Fetching fonts from Google API: ' + error);
+
+      response.status(502).send({ error: "Unable to fetch fonts from Google API!" })
+    }
+  }
+
+  fetchGoogleFonts();
+
   // https://www.googleapis.com/webfonts/v1/webfonts?key=YOUR-API-KEY
   // https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity
 
-  response.send([{ name: 'Helvetica' }])
+  // response.send([{ name: 'Helvetica' }])
 }); 
 
 module.exports = router;
