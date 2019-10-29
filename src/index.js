5aef5f97d6ed2b409f81ae5bb95dca96ec78840e
Will Gutierrez <will.gutierrez@gmail.com>
2019-10-28 17:52:29 -0700

    finish first draft of initial font fetch with react useEffect hook

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 function FontsContainer() {
   const [fonts, setFonts] = useState([]);
 
-  
+  useEffect(() => {
+    const fetchFonts = async() => {
+      const result = await axios('/api/get_all_fonts');
+      setFonts(result.data);    
+    }
+
+    fetchFonts();
+  }, [fonts]);
+
+  // {fonts.map((item, index) => (
+  //   <li key={item.index}>
+  //     {item.name}
+  //   </li>
+  // ))}
+
+  return (
+    <div>
+      {fonts ? (
+        Fonts are here!
+      ) : (
+        Loading...
+      )}
+    </div>
+  );
 }
 
-// https://www.googleapis.com/webfonts/v1/webfonts?key=YOUR-API-KEY
-// https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity
-
-// ReactDOM.render(
-//   <div>{title}</div>,
-//   document.getElementById('app')
-// );
+ReactDOM.render(
+  <FontsContainer />,
+  document.getElementById('app')
+);
