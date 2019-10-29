57dc1b186646b7005986780fbfad686644fb4e1e
Will Gutierrez <will.gutierrez@gmail.com>
2019-10-29 12:50:12 -0700

    change the response for /get_all_fonts request

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,7 +8,7 @@ router.get('/get_all_fonts', (request, response, next) => {
   // https://www.googleapis.com/webfonts/v1/webfonts?key=YOUR-API-KEY
   // https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity
 
-  response.send({ data: [{ name: 'Helvetica' }] })
+  response.send([{ name: 'Helvetica' }])
 }); 
 
 module.exports = router;
