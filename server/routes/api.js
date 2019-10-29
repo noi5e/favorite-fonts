c43497163741380e2e66c1721de83248a3868a40
Will Gutierrez <will.gutierrez@gmail.com>
2019-10-28 17:51:57 -0700

    write a dummy /get_all_fonts API

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,6 +8,7 @@ router.get('/get_all_fonts', (request, response, next) => {
   // https://www.googleapis.com/webfonts/v1/webfonts?key=YOUR-API-KEY
   // https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity
 
+  response.send({ data: [{ name: 'Helvetica' }] })
 }); 
 
 module.exports = router;
