6735cad3500e25c150d3adc508dcf83b3f522aa3
Will Gutierrez <will.gutierrez@gmail.com>
2019-11-16 21:37:12 -0800

    move HTML and styled divs up from Main.jsx to index.js

--- src/index.js
+++ src/index.js
@@ -6,26 +6,36 @@ import axios from 'axios';
 // import store from './redux/store';
 
 import Main from './components/container/Main.jsx';
-import FontCard from './components/presentational/FontCard.jsx';
-
-function App() {
-  const [fonts, setFonts] = useState([]);
-
-  useEffect(() => {
-    const fetchFonts = async() => {
-      const result = await axios('/api/get_all_fonts');
-      setFonts(result.data.slice(0, 8));
-    }
-
-    fetchFonts();
-  }, []);
-
-  return (
-    <Main fonts={fonts} />
-  );
-}
 
 ReactDOM.render(
-  <App />,
+  // <Provider store={store}>
+    <div id="container">
+      <div id="header-container">
+        <div id="header-logo"><b>Qoogle</b> Fonts</div>
+        <nav id="nav">
+          <a id="nav-link">Catalog</a>
+          <a id="nav-link">Featured</a>
+          <a id="nav-link">Articles</a>
+          <a id="nav-link">About</a>
+        </nav>
+        <div id="search-tools">
+          <div className="input-group">
+            <input id="search-input" type="search" aria-label="search fonts" className="form-control" placeholder="Search fonts..." />
+            <input id="sample-text-input" type="text" aria-label="enter sample text" className="form-control" placeholder="Type something!" />
+            <select id="font-size-select" className="custom-select">
+              <option selected value="24px">24px</option>
+              <option value="40px">40px</option>
+              <option value="64px">64px</option>
+              <option value="96px">96px</option>
+            </select>
+            <div className="input-group-append">
+              <button id="reset-button" className="btn btn-outline-secondary" type="button"><i className="fas fa-sync-alt"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Main />
+    </div>,
   document.getElementById('app')
 );
+
