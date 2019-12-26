2760fd0523d4c0a245e4d68f5305ef2c454c727e
Will Gutierrez <will.gutierrez@gmail.com>
2019-12-26 15:45:00 -0800

    create individual component for login button and login container functions

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import SettingsContainer from './SettingsContainer.jsx';
+import LoginContainer from './LoginContainer.jsx';
 
 const Header = props => {
   return (
     <div id="header-container">
       <div id="header-logo"><b>Qoogle</b> Fonts</div>
-      <nav id="nav">
-        <a href="/auth/google" id="nav-link">Sign in with Google</a>
-      </nav>
+      <LoginContainer />
       <SettingsContainer />
     </div>  
     );
---
+++ client/src/pages/LoginContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginContainer = props => {
+  return (
+    <nav id="nav">
+      <a href="/auth/google" id="nav-link">Sign in with Google</a>
+    </nav>
+  );
+}
+
+export default LoginContainer;
