b142c719297de89fa9c9c99230f97fb17ee7642a
Will Gutierrez <will.gutierrez@gmail.com>
2019-12-27 17:55:47 -0800

    configure express-session, and Postgres session store

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const server = require('http').Server(app);
 
 // socket.io
 const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// set socket.io in the express app, so it can be accessed within auth.js
+app.set('io', io);
 
 // initialize passport & load passport strategy
 const passport = require('passport');
@@ -27,7 +33,25 @@ const knex = require('knex')({
     password: process.env.POSTGRES_PASSWORD,
     database: 'favorite-fonts'
   }
-})
+});
+
+// express-session
+const session = require('express-session');
+
+// connect-pg-simple: for storing Express session data in Postgres
+// we need this to save web socket IDs to the request.
+// the app uses web sockets to forward JSON web tokens to client instances once they are authenticated.
+const pgSession = require('connect-pg-simple')(session);
+
+app.use(session({
+  resave: false,
+  secret: process.env.SESSION_SECRET,
+  saveUninitialized: true,
+  store: new pgSession({
+    conString: process.env.POSTGRES_CONNECTION_STRING,
+    ttl: 7 * 24 * 60 * 60 // one week
+  })
+}));
 
 // static files
 app.use(express.static('./dist'));
@@ -47,4 +71,4 @@ const port = process.env.PORT || 8080;
 
 server.listen(port, function() {
   console.log('App listening on port ' + port + '!');
-})
+});
