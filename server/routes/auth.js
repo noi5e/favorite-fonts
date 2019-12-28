40d3ca76231ee67d5ae91e14fa4e3db4e8951d06
Will Gutierrez <will.gutierrez@gmail.com>
2019-12-27 17:57:29 -0800

    write middleware that adds websocket id to request session, change successful login route so server sends token via socket.io

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = new express.Router();
 const passport = require('passport');
 
-router.get('/google', passport.authenticate('google-oauth', { scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'] }));
+const addSocketToSession = function(request, response, next) {
+  request.session.socketId = request.query.socketId;
+  next();
+};
+
+router.get('/google', addSocketToSession, passport.authenticate('google-oauth', { scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'] }));
 
 router.get('/google/callback', passport.authenticate('google-oauth'), function(request, response) {
-  response.setHeader('Authorization', 'Bearer ' + request.user.token);
+  const io = request.app.get('io');
+  io.to(`${request.session.socketId}`).emit('login-success', 'fake token!');
+
   response.redirect(process.env.APP_URL);
 });
 
