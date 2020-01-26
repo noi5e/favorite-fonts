3d70e5cd80f787eefa599688dae2f1b100a67c89
Will Gutierrez <will.gutierrez@gmail.com>
2020-01-26 12:10:29 -0800

    remove commented out code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,13 +10,6 @@ const addSocketToSession = function(request, response, next) {
 
 router.get('/google', addSocketToSession, passport.authenticate('google-oauth', { scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'] }));
 
-// router.get('/google/callback', passport.authenticate('google-oauth'), function(request, response) {
-//   const io = request.app.get('io');
-//   io.to(`${request.session.socketId}`).emit('login-success', 'fake token!');
-
-//   response.redirect(process.env.APP_URL);
-// });
-
 router.get('/google/callback', function(request, response, next) {
   passport.authenticate('google-oauth', function(error, user) {
     if (error) { console.log(error); };
