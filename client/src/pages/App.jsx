7ebc232e433b7992f29f48bf02f60a8f649cfa48
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-03 15:37:47 -0800

    create new component App.jsx from index.js

---
+++ client/src/pages/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header.jsx";
+import FontCardContainer from "./FontCardContainer.jsx";
+
+const App = props => {
+  return (
+    <div id="container">
+      <Header />
+      <FontCardContainer />
+    </div>
+  );
+};
+
+export default App;
