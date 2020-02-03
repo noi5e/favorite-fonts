eb17214be8eac62ea93295c3e1d208ba4e7f8f91
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-03 15:38:54 -0800

    implement conditional rendering of sign-in button depending on user authentication state

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import SettingsContainer from './SettingsContainer.jsx';
 import LoginContainer from './LoginContainer.jsx';
 
-const Header = props => {
+const Header = ({ user }) => {
   return (
     <div id="header-container">
       <div id="header-logo"><b>Qoogle</b> Fonts</div>
-      <LoginContainer />
+      {(Object.keys(user).length === 0 && user.constructor === Object) && <LoginContainer />}
       <SettingsContainer />
     </div>  
     );
 };
 
-export default Header;
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(
+  mapStateToProps
+)(Header);
