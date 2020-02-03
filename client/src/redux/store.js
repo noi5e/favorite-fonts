1e4189e00445394515e9c7bf873f946e32f26109
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-03 15:38:11 -0800

    install and import redux-logger

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from 'redux-thunk';
+import { createLogger } from "redux-logger";
 import { requestFonts, receiveFonts, fetchFonts, userLoginSuccess } from './actions';
 import reducer from './reducers';
 
-export default createStore(reducer, applyMiddleware(thunkMiddleware));
+const middlewares = [thunkMiddleware];
+
+if (process.env.NODE_ENV === "development") {
+  const loggerMiddleware = createLogger();
+  middlewares.push(loggerMiddleware);
+}
+
+export default createStore(reducer, applyMiddleware(...middlewares));
