6eb2d1006051cc3401d23e01eba2f529c43a6297
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-03 20:44:55 -0800

    separate out authenticated header links from Header.jsx

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -3,22 +3,14 @@ import { connect } from 'react-redux';
 
 import SettingsContainer from './SettingsContainer.jsx';
 import LoginContainer from './LoginContainer.jsx';
-
-const ProfileLink = props => {
-  return (
-    <nav id="nav">
-      <a className="nav-link auth-link">Your Favorite Fonts</a>
-      <a className="nav-link auth-link">Logout</a>
-    </nav>
-  );
-}
+import AuthHeader from "./AuthHeader.jsx";
 
 const Header = ({ user }) => {
   return (
     <div id="header-container">
       <div id="header-logo"><b>Qoogle</b> Fonts</div>
       {/* Syntax below checks for if the user object in state is empty or not */}
-      {(Object.keys(user).length === 0 && user.constructor === Object) ? <LoginContainer /> : <ProfileLink firstName={user.firstName} />}
+      {(Object.keys(user).length === 0 && user.constructor === Object) ? <LoginContainer /> : <AuthHeader />}
       <SettingsContainer />
     </div>  
     );
