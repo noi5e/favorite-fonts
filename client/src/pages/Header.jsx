96bd20a0b82be42c7c6ef297456006d21fe5a16d
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-03 20:13:16 -0800

    display authenticated nav links

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -4,11 +4,21 @@ import { connect } from 'react-redux';
 import SettingsContainer from './SettingsContainer.jsx';
 import LoginContainer from './LoginContainer.jsx';
 
+const ProfileLink = props => {
+  return (
+    <nav id="nav">
+      <a className="nav-link auth-link">Your Favorite Fonts</a>
+      <a className="nav-link auth-link">Logout</a>
+    </nav>
+  );
+}
+
 const Header = ({ user }) => {
   return (
     <div id="header-container">
       <div id="header-logo"><b>Qoogle</b> Fonts</div>
-      {(Object.keys(user).length === 0 && user.constructor === Object) && <LoginContainer />}
+      {/* Syntax below checks for if the user object in state is empty or not */}
+      {(Object.keys(user).length === 0 && user.constructor === Object) ? <LoginContainer /> : <ProfileLink firstName={user.firstName} />}
       <SettingsContainer />
     </div>  
     );
