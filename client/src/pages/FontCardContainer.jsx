a718e9ae5f253def60e69e4e4e7da5696cc340c1
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-17 20:02:07 -0800

    write POST request to /api/toggle_font_like, and Prettify code

--- client/src/pages/FontCardContainer.jsx
+++ client/src/pages/FontCardContainer.jsx
@@ -1,31 +1,45 @@
-import React, { useEffect} from 'react';
-import { connect } from 'react-redux';
-import { fetchFonts } from '../redux/actions';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
 
-import FontCard from './FontCard.jsx';
+import { fetchFonts } from "../redux/actions";
+import { isUserAuthenticated, getUser } from "../utilities/authentication";
+import FontCard from "./FontCard.jsx";
 
 const FontCardContainer = ({ fonts, fetchFonts, isFetching }) => {
-
   useEffect(() => {
     if (fonts.length === 0) {
       fetchFonts();
     }
   });
 
-  const fontCards = !isFetching ? (
-    fonts.map((font, index) => (
-      <FontCard key={index} fontName={font.family} author={'Christian Robertson'} sampleText={'The quick brown fox jumped over the lazy dog.'} />
-    ))
-  ) : (
-    "Loading..." 
-  );
+  const handleFave = fontName => {
+    (async () => {
+      const result = await axios({
+        url: "/api/toggle_font_fave",
+        method: "post",
+        headers: { Authorization: `Bearer ${JSON.parse(getUser()).token}` },
+        data: { fontName }
+      });
 
-  return (
-    <div id="font-card-container">
-      {fontCards}
-    </div>
-  );
-}
+      console.log(result);
+    })();
+  };
+
+  const fontCards = !isFetching
+    ? fonts.map((font, index) => (
+        <FontCard
+          key={index}
+          fontName={font.family}
+          author={"Christian Robertson"}
+          handleFave={handleFave}
+          sampleText={"The quick brown fox jumped over the lazy dog."}
+        />
+      ))
+    : "Loading...";
+
+  return <div id="font-card-container">{fontCards}</div>;
+};
 
 const mapStateToProps = state => ({
   fonts: state.fonts,
@@ -34,7 +48,4 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = { fetchFonts };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(FontCardContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(FontCardContainer);
