2f21d15cfd83a6d35d6dcbf1d40b1f68e0650408
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-17 20:04:43 -0800

    write onClick for heart button

--- client/src/pages/FontCard.jsx
+++ client/src/pages/FontCard.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
 export default function FontCard(props) {
-
   const query = props.fontName.split(" ").join("+");
   const link = `https://fonts.googleapis.com/css?family=${query}&display=swap`;
 
@@ -17,16 +16,22 @@ export default function FontCard(props) {
           onload="if(media!='all')media='all'"
         />
       </Helmet>
-      <i className="far fa-heart like-button"></i>
+      <i
+        className="far fa-heart like-button"
+        onClick={() => {
+          props.handleFave(props.fontName);
+        }}
+      ></i>
       <div className="font-name">{props.fontName}</div>
-      <div className="sample-text" style={{ fontFamily: `${props.fontName}` }}>{props.sampleText}</div>
+      <div className="sample-text" style={{ fontFamily: `${props.fontName}` }}>
+        {props.sampleText}
+      </div>
     </div>
   );
-  
 }
 
 FontCard.propTypes = {
   fontName: PropTypes.string.isRequired,
   // fontSize: PropTypes.num.isRequired,
   sampleText: PropTypes.string.isRequired
-}
+};
