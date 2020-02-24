7822bf3e55ba8eab32fc2b6e8bb5d1409bc31ec8
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-23 20:47:55 -0800

    prettify

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,29 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const addSocketToSession = function(request, response, next) {
   request.session.socketId = request.query.socketId;
   next();
 };
 
-router.get('/google', addSocketToSession, passport.authenticate('google-oauth', { scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile'] }));
+router.get(
+  "/google",
+  addSocketToSession,
+  passport.authenticate("google-oauth", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile"
+    ]
+  })
+);
 
-router.get('/google/callback', function(request, response, next) {
-  passport.authenticate('google-oauth', function(error, user) {
-    if (error) { console.log(error); }
+router.get("/google/callback", function(request, response, next) {
+  passport.authenticate("google-oauth", function(error, user) {
+    if (error) {
+      console.log(error);
+    }
 
-    const token = jwt.sign({
-      sub: user.user_id
-    }, process.env.JWT_KEY);
+    const token = jwt.sign(
+      {
+        sub: user.user_id
+      },
+      process.env.JWT_KEY
+    );
 
-    const io = request.app.get('io');
-    io.to(`${request.session.socketId}`).emit('login-success', {
+    const io = request.app.get("io");
+    io.to(`${request.session.socketId}`).emit("login-success", {
       token,
       firstName: user.first_name
     });
-
   })(request, response, next);
 });
 
