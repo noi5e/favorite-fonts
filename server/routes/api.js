66d0ad7ef18e4bc5c04659d478a19a5939d74631
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-23 20:47:12 -0800

    write new API endpoint for requesting user faves

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,48 @@ const jwt = require("jsonwebtoken");
 
 const db = require("../../db");
 
-router.get("/get_all_fonts", (request, response, next) => {
+router.post("/get_user_faves", (request, response) => {
+  (async () => {
+    const client = await db.connect();
+
+    try {
+      const jsonWebToken = request.headers.authorization.split(" ")[1];
+
+      const userID = await jwt.verify(
+        jsonWebToken,
+        process.env.JWT_KEY,
+        (error, decodedToken) => {
+          return decodedToken.sub;
+        }
+      );
+
+      const userTokenIsValidQuery = await client.query(
+        "SELECT * FROM users WHERE user_id = $1",
+        [userID]
+      );
+
+      if (userTokenIsValidQuery.rows.length > 0) {
+        const faveFontsQuery = await client.query(
+          "SELECT * FROM font_likes WHERE parent_id = $1",
+          [userID]
+        );
+
+        response
+          .status(200)
+          .send(faveFontsQuery.rows.map(font => ({ family: font.font_id })));
+      } else {
+        response.status(401).end();
+      }
+    } finally {
+      client.release();
+    }
+  })().catch(error => {
+    console.log(error);
+    response.status(500).end();
+  });
+});
+
+router.get("/get_all_fonts", (request, response) => {
   const fetchGoogleFonts = async () => {
     try {
       const result = await axios.get(
@@ -18,7 +59,9 @@ router.get("/get_all_fonts", (request, response, next) => {
         }
       );
 
-      response.send(result.data.items);
+      response.send(
+        result.data.items.map(item => ({ family: item.family, liked: false }))
+      );
     } catch (error) {
       console.log("Fetching fonts from Google API: " + error);
       response
