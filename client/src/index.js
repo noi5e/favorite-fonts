8f103c61428462203f9b55fcbff7496a41169290
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-26 23:27:41 -0800

    prettify

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+/* eslint-disable react/prop-types */
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-import App from './pages/App.jsx';
-import FontCardContainer from './pages/FontCardContainer.jsx';
-import Header from './pages/Header.jsx';
+import App from "./pages/App.jsx";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
