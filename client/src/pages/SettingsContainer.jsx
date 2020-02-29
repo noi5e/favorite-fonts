52f2fb96026bdbd186bb7c35141b474574ce632e
Will Gutierrez <will.gutierrez@gmail.com>
2020-02-28 20:21:35 -0800

    add action to update sample text

--- client/src/pages/SettingsContainer.jsx
+++ client/src/pages/SettingsContainer.jsx
@@ -1,21 +1,55 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-const SettingsContainer = props => {
+import { updateSampleText } from "../redux/actions";
+
+const SettingsContainer = ({ updateSampleText }) => {
   return (
     <div id="search-tools" className="input-group">
-      <input id="search-input" type="search" aria-label="search fonts" className="form-control" placeholder="Search fonts..." />
-      <input id="sample-text-input" type="text" aria-label="enter sample text" className="form-control" placeholder="Type something!" />
-      <select id="font-size-select" defaultValue="24px" className="custom-select">
+      <input
+        id="search-input"
+        type="search"
+        aria-label="search fonts"
+        className="form-control"
+        placeholder="Search fonts..."
+      />
+      <input
+        id="sample-text-input"
+        type="text"
+        aria-label="enter sample text"
+        className="form-control"
+        placeholder="Type something!"
+        onChange={e => updateSampleText(e.target.value)}
+      />
+      <select
+        id="font-size-select"
+        defaultValue="24px"
+        className="custom-select"
+      >
         <option value="24px">24px</option>
         <option value="40px">40px</option>
         <option value="64px">64px</option>
         <option value="96px">96px</option>
       </select>
       <div className="input-group-append">
-        <button id="reset-button" className="btn btn-outline-secondary" type="button"><i className="fas fa-sync-alt"></i></button>
+        <button
+          id="reset-button"
+          className="btn btn-outline-secondary"
+          type="button"
+        >
+          <i className="fas fa-sync-alt"></i>
+        </button>
       </div>
     </div>
   );
 };
 
-export default SettingsContainer;
+const mapStateToProps = state => ({
+  sampleText: state.sampleText
+});
+
+const mapDispatchToProps = {
+  updateSampleText
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SettingsContainer);
