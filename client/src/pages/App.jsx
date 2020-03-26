bbb3154a10d9a22236bf5eb22e468c2682a12e32
Will Gutierrez <will.gutierrez@gmail.com>
2020-03-25 20:50:38 -0700

    scroll window to top if user enters searchTerm

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -9,7 +9,7 @@ import Header from "./Header.jsx";
 import FavoritesContainer from "./FavoritesContainer.jsx";
 import FontCardContainer from "./FontCardContainer.jsx";
 
-const App = ({ userLoginSuccess }) => {
+const App = ({ userLoginSuccess, searchTerm }) => {
   useEffect(() => {
     if (isUserAuthenticated()) {
       const firstName = JSON.parse(getUser()).firstName;
@@ -17,19 +17,27 @@ const App = ({ userLoginSuccess }) => {
     }
   });
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [searchTerm]);
+
   return (
     <div id="container">
       <Router>
         <Header />
-          <Switch>
-            <Route exact path="/" component={FontCardContainer} />
-            <Route path="/favorites" component={FavoritesContainer} />
-          </Switch>
-        </Router>
+        <Switch>
+          <Route exact path="/" component={FontCardContainer} />
+          <Route path="/favorites" component={FavoritesContainer} />
+        </Switch>
+      </Router>
     </div>
   );
 };
 
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
 const mapDispatchToProps = { userLoginSuccess };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
