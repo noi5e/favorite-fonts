965807faf059801391fc3627d096a3d7123be88f
Will Gutierrez <will.gutierrez@gmail.com>
2020-03-30 16:31:52 -0700

    pass down fontSize from state

--- client/src/pages/FavoritesContainer.jsx
+++ client/src/pages/FavoritesContainer.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import axios from "axios";
 import { connect } from "react-redux";
 import debounce from "lodash.debounce";
@@ -11,13 +12,14 @@ import {
   likeFont,
   loadFavoriteFonts
 } from "../redux/actions";
-import { getUser } from "../utilities/authentication";
+import { getUser, isUserAuthenticated } from "../utilities/authentication";
 
 const FavoritesContainer = ({
   likeFont,
   dislikeFont,
   displayedFonts,
   displayMoreFonts,
+  fontSize,
   sampleText,
   loadFavoriteFonts,
   moreFontsToDisplay,
@@ -62,13 +64,19 @@ const FavoritesContainer = ({
     })();
   };
 
-  if (!searchMatchesFonts) {
+  console.log(isUserAuthenticated());
+
+  if (!isUserAuthenticated()) { 
+    console.log("user isn't authenticated");
+    return <Redirect to="/" />
+  } else if (!searchMatchesFonts) {
     return <div id="message">No matching fonts found.</div>;
   } else {
     const favoriteFonts = displayedFonts.map((font, index) => (
       <FontCard
         key={index}
         fontName={font.family}
+        fontSize={fontSize}
         handleFave={handleFave}
         sampleText={sampleText}
         liked={font.liked}
@@ -82,6 +90,7 @@ const FavoritesContainer = ({
 const mapStateToProps = state => ({
   displayedFonts: state.displayedFonts,
   fonts: state.fonts,
+  fontSize: state.fontSize,
   sampleText: state.sampleText,
   moreFontsToDisplay: state.moreFontsToDisplay,
   searchMatchesFonts: state.searchMatchesFonts
